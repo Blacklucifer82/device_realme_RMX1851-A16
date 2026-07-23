af490f7041e37226b7f047a5bead38d76cec485e
Blacklucifer82 <sourabhkori82@gmail.com>
2026-07-24 00:32:12 +0530

    RMX1851: PixelOS-ify @PixelOS-AOSP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/infinity_RMX1851.mk
+    $(LOCAL_DIR)/custom_RMX1851.mk
 
 COMMON_LUNCH_CHOICES := \
-    infinity_RMX1851-userdebug \
-    infinity_RMX1851-eng \
-    infinity_RMX1851-user
+    custom_RMX1851-userdebug \
+    custom_RMX1851-eng \
+    custom_RMX1851-user
